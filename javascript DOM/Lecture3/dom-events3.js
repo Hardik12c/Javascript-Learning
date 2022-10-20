0a67124eb7eea465c8f10d646b06f336f5df3aab
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-10-20 19:25:48 +0530

    wishlist app completed

--- javascript DOM/Lecture3/dom-events3.js
+++ javascript DOM/Lecture3/dom-events3.js
@@ -1,12 +1,12 @@
 let input=document.querySelector('.input');
-var btn=document.querySelector('.addBtn');
+let btn=document.querySelector('.addBtn');
 let ul=document.querySelector('ul');
-let span=document.createElement('span');
-let li=document.createElement('li');
-
-
+let list=Array.from(document.querySelectorAll('.element'));
+let spanelement=Array.from(document.querySelectorAll('.close'));
 let appdnfunction=()=>{
     if(input.value!=""){
+        let span=document.createElement('span');
+        let li=document.createElement('li');
         li.className="element";
         li.setAttribute('id','elementId');
         li.innerText=input.value;
@@ -19,11 +19,23 @@ let appdnfunction=()=>{
         alert("task bar is empty");
     }
 }
-btn.addEventListener('click',appdnfunction());
 
+
+btn.addEventListener('click',appdnfunction);
 input.addEventListener('keydown',(e)=>{
     if(e.keyCode=='13'){
         appdnfunction();
     }
 })
 
+list.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        e.classList.toggle('checked');
+    })
+})
+
+spanelement.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        e.parentElement.remove();
+    })
+})
